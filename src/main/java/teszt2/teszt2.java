dcca934f43010cc34a32005c221f972086bd57cf
AreYouScorpio <akos.schweger@gmail.com>
2022-06-21 12:29:37 +0200

    aliz2

--- src/main/java/teszt2/teszt2.java
+++ src/main/java/teszt2/teszt2.java
@@ -1,9 +1,12 @@
 package teszt2;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,46 +25,41 @@ public class teszt2 {
     }
 
 
-
     void readFile(Path p) throws IllegalStateException {
         try {
             List<String> lines = Files.readAllLines(p);
             int lineNumber = 1;
             for (String line : lines) {
                 String strings[] = line.split(" ");
-                for(String s:strings) {
-                    if(!words.containsKey(s))
+                for (String s : strings) {
+                    if (!words.containsKey(s))
                         words.put(s, new ArrayList());
                     words.get(s).add(lineNumber);
                 }
                 lineNumber++;
             }
-        }catch(IOException ise)
-        {
-            throw new IllegalStateException("Can't read file!",ise);
+        } catch (IOException ise) {
+            throw new IllegalStateException("Can't read file!", ise);
         }
     }
 
 
     void writeFile(Path p) throws IOException {
         FileWriter outFile = new FileWriter(p.toString());
-        for (H h : h) {
-            char first = h.getIdentityNumber().charAt(0);
-            if (first == '1' || first == '3') {
-                outFile.write(h.getName());
-                outFile.write(";");
-                outFile.write(h.getIdentityNumber());
-                outFile.write("\n");
-            }
+        for (Map.Entry<String, List> entry : words.entrySet()) {
+            String word = entry.getKey();
+            outFile.write(word);
+            outFile.write(' ');
+            outFile.write(entry.getValue().toString());
         }
         outFile.close();
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         teszt2 teszt2 = new teszt2();
-
-
+        teszt2.readFile(Path.of("example.txt"));
+        teszt2.writeFile(Path.of("example-index.txt"));
     }
 
 }
